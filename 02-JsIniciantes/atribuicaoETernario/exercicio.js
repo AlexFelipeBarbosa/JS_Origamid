c2536618316591296b61260c857ddb86b973f1f4
Alex Barbosa <alexfelipebarbosa@yahoo.com.br>
2020-08-28 11:52:33 -0300

    Operadores de Atribuição e Ternario

---
+++ 02-JsIniciantes/atribuicaoETernario/exercicio.js
@@ -0,0 +1,21 @@
+console.log(" ");
+console.log("Exercicio");
+console.log(" ");
+
+/* Some 500 ao valor do scroll abaixo
+atribuindo o novo valor do scroll
+*/
+var scroll = 1000;
+scroll += 500;
+console.log(scroll);
+
+/* Atribuia true para a variavel darCredito
+caso o cliente possua carro e casa 
+e false caso contrario
+*/
+var possuiCarro = true;
+var possuiCasa = true;
+var darCredito;
+
+darCredito = possuiCarro && possuiCasa;
+console.log(darCredito);
